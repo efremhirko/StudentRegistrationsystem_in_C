3bb46b4691d5327788815aafa589cf18cc755f58
efremhirko <119780561+efremhirko@users.noreply.github.com>
2023-11-05 19:52:55 +0300

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+    }
+    
+}
